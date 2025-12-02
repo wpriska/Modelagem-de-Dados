8bb0243b33398e77222949d65b88d5a577bd2dc1
Wellington Machado Bezerra <wpriska@gmail.com>
2025-12-01 21:32:32 -0300

    Update 04_UpdateCommands.sql

--- ClinicaVeterinaria/04_UpdateCommands.sql
+++ ClinicaVeterinaria/04_UpdateCommands.sql
@@ -1,16 +1,19 @@
 
--- UPDATE O NOME DO MEDICAMENTO NA TABELA MEDICAMENTO PELO SEU ID  ---
+-- UPDATE O NOME DO MEDICAMENTO NA TABELA PELO SEU ID  ---
 
 UPDATE Medicamento
     SET nome = 'dipirona'
     WHERE id_medicamento = 2;
 
+
+-- UPDATE O NOME DO PET NA TABELA PELO SEU ID  ---
 UPDATE Pet
     SET nome = 'Gato'
     WHERE id_pet = 2;
 
-
+-- UPDATE O NOME DO VETERINARIO NA TABELA PELO SEU ID  ---
 UPDATE Veterinario
     SET nome = 'Lucas da Silva'
     WHERE id_medicamento = 1;
+
 
