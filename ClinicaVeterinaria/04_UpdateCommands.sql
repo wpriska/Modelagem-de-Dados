644bd9e28d62b3b23b1fb99606a8a5635bb9dca9
Wellington Machado Bezerra <wpriska@gmail.com>
2025-12-01 21:13:22 -0300

    Update 04_UpdateCommands.sql

--- ClinicaVeterinaria/04_UpdateCommands.sql
+++ ClinicaVeterinaria/04_UpdateCommands.sql
@@ -5,3 +5,12 @@ UPDATE Medicamento
     SET nome = 'dipirona'
     WHERE id_medicamento = 2;
 
+UPDATE Pet
+    SET nome = 'Gato'
+    WHERE id_pet = 2;
+
+
+UPDATE Veterinario
+    SET nome = 'Lucas da Silva'
+    WHERE id_medicamento = 1;
+
